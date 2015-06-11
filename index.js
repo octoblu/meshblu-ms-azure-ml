e51aa6c90456591a6b8823c29fc3b963c40870b8
Sqrt of Saturn <sqrt@octoblu.com>
2015-06-11 15:10:15 -0700

    Initial commit, allows you to connect to a Microsoft Azure ML experiment web service and connect it to Octoblu

---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
+var debug = require('debug')('meshblu-microsoft-ml');
+var request = require('request');
+
+var MESSAGE_SCHEMA = {
+  type: 'object',
+  properties: {
+    Inputs: {
+      type: 'object',
+      required: true
+    }
+  }
+};
+
+var OPTIONS_SCHEMA = {
+  type: 'object',
+  properties: {
+      endpointUrl: {
+      type: 'string',
+      required: true
+    },
+    apiKey : {
+      type : 'string',
+      required: true,
+      format: 'password'
+    }
+  }
+};
+
+function Plugin(){
+  this.options = {
+    endPointUrl : 'http://www.example.com',
+    apiKey : ''
+  };
+  this.messageSchema = MESSAGE_SCHEMA;
+  this.optionsSchema = OPTIONS_SCHEMA;
+  return this;
+}
+util.inherits(Plugin, EventEmitter);
+
+Plugin.prototype.onMessage = function(message){
+  var self = this;
+  var payload = message.payload;
+  console.log(message);
+  console.log('self', self.options);
+  if(message.payload){
+    request({
+      method : 'POST',
+      uri : self.options.endpointUrl,
+      json: true,
+      auth : {
+        bearer : self.options.apiKey
+      },
+      header: {
+        "Content-Type": "application/json",
+        "Content-Length": JSON.stringify(message.payload.Inputs).length,
+        "Accept": "application/json"
+      },
+      body: message.payload.Inputs
+    }, function(error, response, body){
+      debug('After web service');
+      debug(response);
+      debug(body);
+
+      if(error){
+         self.emit('error', error);
+      }
+      self.emit('data', body);
+    });
+  }
+};
+
+Plugin.prototype.onConfig = function(device){
+  this.setOptions(device.options||{});
+};
+
+Plugin.prototype.setOptions = function(options){
+  this.options = options;
+};
+
+module.exports = {
+  messageSchema: MESSAGE_SCHEMA,
+  optionsSchema: OPTIONS_SCHEMA,
+  Plugin: Plugin
+};
